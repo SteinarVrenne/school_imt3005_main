b580dd209d8eeee52f055ae0442a1b37be6f100a
Harald <haraar@stud.ntnu.no>
2020-10-19 16:52:48 +0200

    Added files from previous reposetory. Some docker commit and dockerfiles for vnc and linux instances.

---
+++ Project/vnc/guacamole/guacamole-dependencies.sh
@@ -0,0 +1,19 @@
+sudo apt install libcairo2-dev
+
+if debian
+    sudo apt install libjpeg62-turbo-dev
+
+if ubuntu
+    sudo apt install libjpeg-turbo8-dev
+
+
+sudo apt install libpng-dev
+
+
+sudo apt install libtool-bin
+
+
+sudo apt install libossp-uuid-dev
+
+# Source
+# https://guacamole.apache.org/doc/1.1.0/gug/installing-guacamole.html
