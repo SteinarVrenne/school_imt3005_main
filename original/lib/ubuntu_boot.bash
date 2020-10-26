aa7d5d1473f8702937e46fe41a330f367ef26266
jimbob <abdifatah87@live.no>
2020-10-26 14:58:26 +0100

    added original folder with unedited files

---
+++ original/lib/ubuntu_boot.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -v
+
+export DEBIAN_FRONTEND=noninteractive
+
+# install Puppet agent
+tempdeb=$(mktemp /tmp/debpackage.XXXXXXXXXXXXXXXXXX) || exit 1
+
+i=0;
+wget -O "$tempdeb" https://apt.puppetlabs.com/puppet6-release-bionic.deb
+ret="$?"
+while [ "$i" -lt "5" ] && [ "$ret" -ne "0" ]; do
+  sleep 10
+  wget -O "$tempdeb" https://apt.puppetlabs.com/puppet6-release-bionic.deb
+  ret="$?"
+  let "i++"
+done
+if [ "$ret" -ne "0" ]; then # All attempts to download file failed, instruct clound-init to restart and try again
+  exit 1003
+fi
+
+dpkg -i "$tempdeb"
+apt-get update
+apt-get -y install puppet-agent
+echo "$(/opt/puppetlabs/bin/facter networking.ip) $(hostname).node.consul $(hostname)" >> /etc/hosts
+echo "manager_ip_address manager.node.consul manager" >> /etc/hosts
+/opt/puppetlabs/bin/puppet config set server manager.node.consul --section main
+/opt/puppetlabs/bin/puppet config set runinterval 300 --section main
+/opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
